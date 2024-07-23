516022f7d12d030d9fa93724a82376842211a3ca
Sushant Sigdel <susi_csit2077@lict.edu.np>
2024-07-23 22:33:11 +0545

    Added code to perform a piecewise linear transformation on an image using OpenCV library. It includes reading an image, resizing it, applying the transformation based on specified ranges and slopes, and displaying the transformed image. This feature enhances the image processing capabilities of the application.

---
+++ precewiselineartransformation.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+def pixelVal(pix,r1,s1,r2,s2):
+    if (0 <= pix and pix <= r1):
+        return (s1/r1)*pix
+    elif (r1 < pix and pix <= r2):
+        return ((s2-s1)/(r2-r1)) * (pix-r1) + s1
+    else:
+        return ((255-s2)/(255-r2)) * (pix-r2) + s2
+img = cv2.imread('ghoda.jpg')
+image= cv2.resize(img,(400, 500))
+cv2.imshow('Input Image', image)
+r1 = 70
+s1 = 0
+r2 = 255
+s2 = 255
+pixelVal_vec = np.vectorize(pixelVal)
+cont_st_img = pixelVal_vec(img, r1, s1, r2, s2)
+outimage= cv2.resize(cont_st_img,(400, 500))
+cv2.imshow('Output Image', outimage)
+cv2.waitKey(0)
